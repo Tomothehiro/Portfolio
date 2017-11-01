def546973c28cd8f04d1bb5d35978b4f08544d59
tomo <tomonouchi@hotmail.com>
2017-11-01 13:58:41 -0700

    added rsvp work

--- js/common.js
+++ js/common.js
@@ -1,25 +1,25 @@
-$( document ).ready(function() {
-  var lang = getCookie("lang");
-  if (lang=="en") {
-    showLanguage('en');
-  } else {
-    showLanguage('jp');
-  }
+$(document).ready(function() {
+    var lang = getCookie("lang");
+    if (lang == "jp") {
+        showLanguage('jp');
+    } else {
+        showLanguage('en');
+    }
 });
 
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
 }
 
 function showLanguage(lang) {
-    var langSet = ["jp", "en"];     // 切り替え対象の locale リスト
+    var langSet = ["jp", "en"]; // 切り替え対象の locale リスト
     for (var i = 0, len = langSet.length; i < len; i++) {
         if (lang === langSet[i]) {
             $('.' + langSet[i]).show();
@@ -27,5 +27,5 @@ function showLanguage(lang) {
             $('.' + langSet[i]).hide();
         }
     }
-    document.cookie = "lang=" + lang;  // クッキー保持
+    document.cookie = "lang=" + lang; // クッキー保持
 }
